4cff9d0304acd309641805df3e43f8ed64d602e8
isere1n <0isere1nZ@gmail.com>
2026-09-19 18:13:05 +0800

    实现了玩家的移动逻辑

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ BG_COLOR = (10, 10, 30)
 FPS = 60
 PLAYER_SIZE = 60
 PLAYER_COLOR = (100, 200, 255)
+PLAYER_SPEED = 400
 
 clock = pygame.time.Clock() # 创建一个Clock实例
 player_rect = pygame.rect.Rect((WEIGHT - PLAYER_SIZE) // 2, HEIGHT - 80, PLAYER_SIZE, PLAYER_SIZE)
@@ -22,6 +23,17 @@ while running:
     for event in pygame.event.get(): # 返回一个元素的类型是pygame.event.Event的list
         if event.type == pygame.QUIT: # pygame.event.Event对象都具有一个type属性，表示事件类型
             running =False
+
+    keys = pygame.key.get_pressed() # 返回一个类似布尔序列的对象(ScancodeWrapper)，用 pygame.K_* 索引
+
+    if keys[pygame.K_UP]:
+        player_rect.y -= PLAYER_SPEED // FPS
+    if keys[pygame.K_DOWN]:
+        player_rect.y += PLAYER_SPEED // FPS
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= PLAYER_SPEED // FPS
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += PLAYER_SPEED // FPS
 
     screen.fill(BG_COLOR) # 背景色清屏
     pygame.draw.rect(screen, PLAYER_COLOR, player_rect)

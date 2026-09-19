650742e9c009100663412b01b1d34d8b83baebf0
isere1n <0isere1nZ@gmail.com>
2026-09-19 15:56:42 +0800

    设置背景色，实现每帧背景色清屏和刷新

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import pygame
 pygame.init() # 初始化pygame
 
 WEIGHT, HEIGHT = 800, 600
+BG_COLOR = (10, 10, 30)
 
 screen = pygame.display.set_mode((WEIGHT, HEIGHT)) # 返回一个pygame.surface.Surface对象，副作用是创建窗口
 
 running = True
 while running:
+
     for event in pygame.event.get(): # 返回一个元素的类型是pygame.event.Event的list
         if event.type == pygame.QUIT: # pygame.event.Event对象都具有一个type属性，表示事件类型
             running =False
 
+    screen.fill(BG_COLOR)
+    pygame.display.flip()
 
 
 pygame.quit() # 显式去初始化pygame

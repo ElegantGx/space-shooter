2aa6018d42693f962b7f0e7f9f3d4902d4a139d3
isere1n <0isere1nZ@gmail.com>
2026-09-19 15:28:30 +0800

    实现了游戏窗口的创建

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pygame
 
 pygame.init() # 初始化pygame
 
+WEIGHT, HEIGHT = 800, 600
+
+screen = pygame.display.set_mode((WEIGHT, HEIGHT)) # 返回一个pygame.surface.Surface对象，副作用是创建窗口
+
+running = True
+while running:
+    pass
 
 
 

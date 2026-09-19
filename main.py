f2763de40d4f0973d61c8e9f74992d03cb047bbb
G(x) <2050230094@qq.com>
2026-09-20 01:52:08 +0800

    修复同时按住多个键时出现的BUG

--- main.py
+++ main.py
@@ -22,27 +22,31 @@ while running:
 
     for event in pygame.event.get(): # 返回一个元素的类型是pygame.event.Event的list
         if event.type == pygame.QUIT: # pygame.event.Event对象都具有一个type属性，表示事件类型
-            running =False
+            running = False
 
+    dt = clock.tick(FPS) / 1000.0
     keys = pygame.key.get_pressed() # 返回一个类似布尔序列的对象(ScancodeWrapper)，用 pygame.K_* 索引
 
-    if keys[pygame.K_UP]: # 同时按着多个按键的时候好像有一些bug，待修...
-        player_rect.y -= PLAYER_SPEED // FPS
-    if keys[pygame.K_DOWN]:
-        player_rect.y += PLAYER_SPEED // FPS
-    if keys[pygame.K_LEFT]:
-        player_rect.x -= PLAYER_SPEED // FPS
-    if keys[pygame.K_RIGHT]:
-        player_rect.x += PLAYER_SPEED // FPS
+    dx = 0
+    dy = 0
 
-    if player_rect.top < 0:
-        player_rect.top = 0
-    if player_rect.bottom > HEIGHT:
-        player_rect.bottom = HEIGHT
-    if player_rect.left < 0:
-        player_rect.left = 0
-    if player_rect.right > WIDTH:
-        player_rect.right = WIDTH
+    if keys[pygame.K_UP]:
+        dy -= 1
+    if keys[pygame.K_DOWN]:
+        dy += 1
+    if keys[pygame.K_LEFT]:
+        dx -= 1
+    if keys[pygame.K_RIGHT]:
+        dx += 1
+
+    if dx != 0 and dy != 0:
+        dx *= 0.7071
+        dy *= 0.7071
+
+    player_rect.x += dx * PLAYER_SPEED * dt
+    player_rect.y += dy * PLAYER_SPEED * dt
+
+    player_rect.clamp_ip(screen.get_rect())
 
     screen.fill(BG_COLOR) # 背景色清屏
     pygame.draw.rect(screen, PLAYER_COLOR, player_rect)

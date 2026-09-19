70c0c66b9e5facb4d5e89d0506955b0be51f3758
G(x) <2050230094@qq.com>
2026-09-20 03:35:38 +0800

    添加子弹

--- main.py
+++ main.py
@@ -8,10 +8,58 @@ BG_COLOR = (10, 10, 30)
 FPS = 60
 PLAYER_SIZE = 60
 PLAYER_COLOR = (100, 200, 255)
+BULLET_COLOR = (255, 0 , 0)
 PLAYER_SPEED = 400
+BULLET_SPEED = 600
 
 clock = pygame.time.Clock() # 创建一个Clock实例
-player_rect = pygame.rect.Rect((WIDTH - PLAYER_SIZE) // 2, HEIGHT - 80, PLAYER_SIZE, PLAYER_SIZE)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
+        self.image.fill(PLAYER_COLOR)
+
+        self.rect = self.image.get_rect(midbottom=(WIDTH // 2, HEIGHT - 20))
+
+    def update(self, keys,dt):
+        dx = 0
+        dy = 0
+
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            dy -= 1
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            dy += 1
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            dx -= 1
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            dx += 1
+
+        self.rect.x += dx * PLAYER_SPEED * dt
+        self.rect.y += dy * PLAYER_SPEED * dt
+
+        self.rect.clamp_ip(screen.get_rect())
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, player_x, player_y):
+        super().__init__()
+
+        self.image = pygame.Surface((5,30))
+        self.image.fill(BULLET_COLOR)
+
+        self.rect = self.image.get_rect(center=(player_x, player_y))
+
+    def update(self, dt):
+        self.rect.y -= BULLET_SPEED * dt
+
+        if self.rect.y > HEIGHT:
+            self.kill()
+
+
+player = Player()
+bullets = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group(player)
 
 pygame.display.set_caption("Space Shooter")
 
@@ -27,32 +75,17 @@ while running:
     dt = clock.tick(FPS) / 1000.0
     keys = pygame.key.get_pressed() # 返回一个类似布尔序列的对象(ScancodeWrapper)，用 pygame.K_* 索引
 
-    dx = 0
-    dy = 0
+    player.update(keys, dt)
+    bullets.update(dt)
 
-    if keys[pygame.K_w] or keys[pygame.K_UP]:
-        dy -= 1
-    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        dy += 1
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        dx -= 1
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        dx += 1
-
-    if dx != 0 and dy != 0:
-        dx *= 0.7071
-        dy *= 0.7071
-
-    player_rect.x += dx * PLAYER_SPEED * dt
-    player_rect.y += dy * PLAYER_SPEED * dt
-
-    player_rect.clamp_ip(screen.get_rect())
+    if keys[pygame.K_SPACE]:
+        new_bullet = Bullet(player.rect.centerx, player.rect.top - 15 )
+        bullets.add(new_bullet)
+        all_sprites.add(new_bullet)
 
     screen.fill(BG_COLOR) # 背景色清屏
-    pygame.draw.rect(screen, PLAYER_COLOR, player_rect)
+    all_sprites.draw(screen)
     pygame.display.flip() # 刷新屏幕
-
-    clock.tick(FPS)
 
 
 pygame.quit() # 显式去初始化pygame
